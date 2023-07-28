b4f062131d8b3187831408bff9060ad690dbb4f1
iamkennethburla <kenneth.burla@gmail.com>
2023-07-28 23:40:15 +0800

    deleted validate token strapi type

--- strapi-server/src/api/validate-token/controllers/validate-token.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * validate-token controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::validate-token.validate-token');
--- strapi-server/src/api/validate-token/routes/validate-token.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * validate-token router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::validate-token.validate-token');
