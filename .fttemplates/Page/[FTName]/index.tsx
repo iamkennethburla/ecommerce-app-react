a51983041cd94182daf0136eaee07e28bd77eb2b
iamkennethburla <kenneth.burla@gmail.com>
2023-08-30 03:54:45 +0800

    initial page template

---
+++ .fttemplates/Page/[FTName]/index.tsx
@@ -0,0 +1,17 @@
+// import { IRoute } from '@ecommerce-app/common-core'
+// import { lazy } from 'react'
+// import { Navigate } from 'react-router-dom'
+
+// const Page = lazy(() => import('./Page/Page'))
+
+// const Routes: IRoute[] = [
+//   {
+//     key: 'page',
+//     isRouteProtected: false,
+//     component: Page,
+//     path: '*',
+//     title: 'Page',
+//   },
+// ]
+
+// export default Routes
