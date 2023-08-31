419b8e3ee9182f02bd7949ec8b890a318cef85a8
iamkennethburla <kenneth.burla@gmail.com>
2023-08-31 15:59:32 +0800

    removed user validation

--- apps/admin/src/app/AppBootstrap.tsx
+++ apps/admin/src/app/AppBootstrap.tsx
@@ -1,75 +1,20 @@
 import { Routing } from '@ecommerce-app/common-core'
 import { Routes } from 'apps/admin/src/app/Core/Routing/Routes'
-import { useEffect, useState } from 'react'
-import { useGetUser, useValidateToken } from './Features/Auth/Hooks'
-
-interface IProcessValues {
-  done: boolean
-  loading: boolean
-  error: boolean
-}
-interface IProcessState {
-  tokenValid: IProcessValues
-  userData: IProcessValues
-}
+import { useValidateToken } from './Features/Auth/Hooks'
 
 export function App() {
-  const [processState, setProcessState] = useState<IProcessState>({
-    tokenValid: {
-      done: false,
-      loading: true,
-      error: false,
-    },
-    userData: {
-      done: false,
-      loading: true,
-      error: false,
-    },
-  })
+  const { isValid, isLoading } = useValidateToken()
 
-  const { isValid, isLoading: isValidateTokenLoading } = useValidateToken()
-  const { data: userData, isLoading: isUserLoading } = useGetUser()
-
-  useEffect(() => {
-    if (!isValidateTokenLoading && isValid) {
-      setProcessState((prevState) => ({
-        ...prevState,
-        tokenValid: {
-          ...prevState.tokenValid,
-          loading: false,
-          done: true,
-        },
-      }))
-    }
-  }, [isValid, isValidateTokenLoading])
-
-  useEffect(() => {
-    if (!isUserLoading && userData) {
-      setProcessState((prevState) => ({
-        ...prevState,
-        userData: {
-          ...prevState.userData,
-          loading: false,
-          done: true,
-        },
-      }))
-    }
-  }, [isUserLoading, userData])
-
-  return isProcessesLoading(processState) ? (
+  return isLoading ? (
     <> Loading App</>
   ) : (
     <Routing
       routes={Routes}
       isAuthenticated={isValid}
-      error={false}
+      hasAppLoadError={false}
       userPermissions={[]}
     />
   )
-
-  function isProcessesLoading(processState: IProcessState) {
-    return Object.entries(processState).every(([_, process]) => process.loading)
-  }
 }
 
 export default App
